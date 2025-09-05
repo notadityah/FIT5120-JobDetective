2269114907444772ac6f9907c61f3269eaa8cda6
Aditya Hariharan <aditya12320@gmail.com>
2025-09-05 17:00:17 +1000

    refactor: Remove login route and authentication guard from router

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AnalyzeView from '../views/AnalyzeView.vue'
 import ReportView from '../views/ReportView.vue'
-import LoginView from '../views/LoginView.vue'
 
 // Configure application routes
 const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView,
-  },
   {
     path: '/',
     name: 'home',
@@ -33,21 +27,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
-
-// Global navigation guard for authentication
-router.beforeEach((to, from, next) => {
-  const isAuthenticated = localStorage.getItem('authenticated') === 'true'
-
-  // Redirect to login if not authenticated (except for login page)
-  if (to.name !== 'login' && !isAuthenticated) {
-    next({ name: 'login' })
-    // Redirect to home if already authenticated and trying to access login
-  } else if (to.name === 'login' && isAuthenticated) {
-    next({ name: 'home' })
-  } else {
-    next()
-  }
 })
 
 export default router
